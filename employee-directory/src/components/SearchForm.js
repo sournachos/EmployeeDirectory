b446a1ba541ac70372514b198f845c025ca71c70
sournachos <joserivera18100@gmail.com>
2021-05-19 23:47:01 -0500

    initial commit

---
+++ employee-directory/src/components/SearchForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function SearchForm(props) {
+  return (
+    <form className="container">
+      <div className="form-group mx-5">
+        <h2 className="position-relative" htmlFor="search">Employee Directory</h2>
+        <input
+          className="d-inline"
+          onChange={props.handleInputChange}
+          value={props.search}
+          name="search"
+          type="text"
+          className="form-control"
+          placeholder="Search for a Gif"
+          id="search"
+        />
+        <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
